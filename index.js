fe349141a28d41407aadeedd12b2ad9b75120298
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-03-15 17:08:56 +0300

    made calculator

---
+++ index.js
@@ -0,0 +1,26 @@
+//Basic Math operations
+function add(a,b){
+    return a+b;
+} 
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+//string to number functions
+function makeInt(n){
+    return parseInt(n,10);
+}
+function preserveDecimal(n){
+    return parseFloat(n);
+}
